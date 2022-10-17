7de5be02c8cd0c147d672f62de0bd6e9e0315740
RayyanFv <rayyanfv@gmail.com>
2022-10-17 23:41:38 +0700

    add bottom navigation bar

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -5,10 +5,40 @@ class mainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget pedulyButton() {
+      return FloatingActionButton(onPressed: () {},
+        backgroundColor: Colors.red,
+        child: Image.asset('assets/peduly.png',
+          width: 20,)
+        ,);
+    }
+    
+    Widget customBottomNavBar(){
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        child: BottomAppBar(
+          color: Colors.red,
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              items: [
+            BottomNavigationBarItem(icon: Image.asset('assets/Home.png', width: 21,), label: ''),
+            BottomNavigationBarItem(icon: Image.asset('assets/Search.png', width: 20,),label: ''),
+            BottomNavigationBarItem(icon: Image.asset('assets/document.png', width: 20,),label: ''),
+            BottomNavigationBarItem(icon: Image.asset('assets/Profile.png', width: 20,),label: ''),
+          ]),
+        ),
+      );
+    }
+
     return Scaffold(
+      floatingActionButton: pedulyButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked ,
+      bottomNavigationBar: customBottomNavBar(),
       body: Center(
-        child: 
-        Text('Hai Peduly'),
+        child: Text('Main Page'),
       ),
     );
   }
