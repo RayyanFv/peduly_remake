25bcdaf21becba12d2f3ce2b7da536ca292f27ff
RayyanFv <rayyanfv@gmail.com>
2022-10-18 23:11:35 +0700

    a little add for home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:peduly_remake/shared/theme.dart';
 
 class homePage extends StatelessWidget {
   const homePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Ini adalah home page'),
+
+    Widget header(){
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Hi, Rayyaan',
+                  style: GoogleFonts.poppins(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  Text('Yuk donasi buat mereka yang membutuhkan!',
+                  style: GoogleFonts.poppins(
+                    fontSize: 13,
+                  ),)
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage('assets/Profile.png'))
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+
+
+    return ListView(
+      children: [
+        header(),
+
+      ],
     );
   }
 }
--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = Color.fromRGBO(231, 81, 59, 0);
-
+double defaultMargin = 30.0;
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+);
 
