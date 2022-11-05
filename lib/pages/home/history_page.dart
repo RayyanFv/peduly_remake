2e8451feca0711c541073c6e55c32c7149107089
RayyanFv <rayyanfv@gmail.com>
2022-11-06 00:57:15 +0700

    a little add for home page

--- lib/pages/home/history_page.dart
+++ lib/pages/home/history_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:peduly_remake/widgets/recipe_card.dart';
 
-class historyPage extends StatelessWidget {
+
+class historyPage extends StatefulWidget {
   const historyPage({Key? key}) : super(key: key);
 
   @override
+  State<historyPage> createState() => _historyPageState();
+}
+
+class _historyPageState extends State<historyPage> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ini adalah halaman history'),
-    );
+    return Scaffold(
+        body: recipeCard(
+          title: 'My recipe',
+          rating: '4.9',
+          cookTime: '30 min',
+          thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+        ));
   }
 }
